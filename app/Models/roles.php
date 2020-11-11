1099f183f1646f8e97072e4bf0db6a08c5f3947a
Enrique de jesus Zepeda Ontiveros <quique-43@hotmail.com>
2020-11-10 20:32:51 -0600

    agregcion de roles y algunas pequeñas modificaciones

--- app/Models/roles.php
+++ app/Models/roles.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Materiales extends Model
+class roles extends Model
 {
     use HasFactory;
-
-    protected $table = 'material';
-
-    protected $fillable = ['name', 'cantidad', 'tipo', 'descripcion'. 'costo'];
 }
